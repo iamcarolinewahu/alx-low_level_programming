944cc824eafd8fde988acb0d185711e1c0308dee
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-09-08 13:33:07 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* Main - Entry point
+ *
+ * Description - prints Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0
+ *
+ * /
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+       	return (0);
+}	
