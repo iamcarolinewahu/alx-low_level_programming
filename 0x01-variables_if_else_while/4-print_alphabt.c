25b5cd412d09baf65c3368819b36cf63983f44fc
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-09-09 14:47:06 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet letters in lowercase except q and e
+ *
+ * description: print alphabet letters in lowercase except q and e
+ *
+ * return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	while (1 < 123)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
