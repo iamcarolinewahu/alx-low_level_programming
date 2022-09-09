665463138eec64414bd7f58ea1e39e6d8992ac12
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-09-09 14:24:09 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet letters in both upper and lower case
+ *
+ * description: prints alphabet letters in both upper and lower case
+ *
+ * Return: Always 0(success)
+ */
+
+int main(void)
+{
+	int i = 97;
+	int j = 65;
+
+	while (i < 122)
+	{
+		putchar(i);
+		i++;
+	}
+	while (j < 91)
+	{
+		putchar(j);
+		j++;
+	}
+	putchar(10);
+
+	return (0);
+}
