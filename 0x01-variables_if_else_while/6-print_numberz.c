5b4e1ae2d3e7c434b5b5bfed172502301e6a8eef
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-09-09 15:24:52 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print single digit numbers from 0
+ *
+ * Description: print single digit numbers from 0
+ *
+ * Return: Always 0(success)
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	while (i < 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
