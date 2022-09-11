19ef28cb61e7147c3260b252b7cd55ed36b93f2b
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-09-11 10:51:45 -0700

    advance task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - print tripple digit combinations
+ *
+ * Description: print tripple digit combinations
+ *
+ * Return: Always 0(success)
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48; i <= 55; i++)
+	{
+		for (i = 49; j <= 56; j++)
+		{
+
+			for (k = 50; k <= 57; k++)
+			{
+				if (k > j && j > i)
+				{
+
+					putchar(i);
+					putchar(j);
+					putchar(k);
+
+					if (i < 55 || j < 56 || k < 57)
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+	}
+	putchar(10);
+
+	return (0);
+}
