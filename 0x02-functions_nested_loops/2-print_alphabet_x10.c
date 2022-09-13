d01e2618101ee11e80cf1fb51c993bd18450db6d
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-09-12 22:18:46 -0700

    task 1 & 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/*
+ * print_alphabet: prints lowercase alphabet
+ *
+ */
+
+int print_alphabet(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+	_putchar(i);
+	}
+}
+
+/**
+ * print_alphabet_x10 - ptints alphabet 10 times
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		print_alphabet();
+	}
+}
+
