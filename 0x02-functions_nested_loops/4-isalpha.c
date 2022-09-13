d05ae6cfee38549990235a8465d908355cf4057c
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-09-13 00:14:27 -0700

    fixed error in task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 /**
  * _isalpha - checks for alphabetic character
- * @c is an ascii character
+ * @c: c is an ascii character
  *
  * Return: 1 if c is a letter
  */
@@ -13,8 +13,13 @@ int _isalpha(int c)
 	{
 		return (1);
 	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
 	else
 	{
 		return (0);
 	}
+
 }
