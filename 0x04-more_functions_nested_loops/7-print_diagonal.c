92bd17fa622540360efed955f2ec98afaf84dc1b
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-09-15 17:49:36 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal / line
+ * @n: length of line
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int line = 0;
+
+	while (while line < 0)
+	{
+		_putchar('\');
+		line++;
+	}
+	_putchar('\n');
+}
