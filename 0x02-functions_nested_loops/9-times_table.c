95b2c3743bd9dc82db1a4eca4553682cefadcffa
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-09-18 15:03:28 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int i, j, x;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			x = i * j;
+
+			_putchar(x);
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - calls other functions
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	void times_table(void);
+
+	return (0);
+}
