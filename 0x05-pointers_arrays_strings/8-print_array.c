01eecde08046284aa9256bf69387e2b7e482fc46
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-09-19 16:49:34 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints elements of array
+ * @a: pointer to array
+ * @n: integer
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+		printf("%d, \n", a[i]);
+}
