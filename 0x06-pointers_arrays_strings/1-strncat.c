970247eaf920abbc7c529d8afcb05af621e444ee
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-09-22 10:46:41 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: string 1
+ * @src: string 2
+ * @n - number of values to concatenate
+ *
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i - 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	for (j = 0; j < n; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	if (dest[i - 1] != '\0')
+		dest[i] = '\0';
+
+	return (dest);
+}
