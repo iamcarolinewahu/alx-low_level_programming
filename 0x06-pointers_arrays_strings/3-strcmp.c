23af8a86f205b13303f1db64d3187bc1798c77ed
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-09-22 11:52:55 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,13 +1,13 @@
-#include main.h"
+#include "main.h"
 
 /**
  * _strcmp - compares two strings
  * @s1: first string
  * @s2: second string to compare
- * Description: compares two strings to see which is
+ *
  * Return: -15 if s1 is less than s2, 0 if equal,
  * 15 if s1 is greater
- **/
+ */
 
 int _strcmp(char *s1, char *s2)
 {
