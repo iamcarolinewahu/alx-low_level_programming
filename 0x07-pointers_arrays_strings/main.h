1fbb91c95308770779a3ed72cd6e3c6ae52f36f5
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-09-26 16:22:10 -0700

    task 8

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ void print_chessboard(char (*a)[8]);
 int _putchar(char c);
 unsigned int _strspn(char *s, char *accept);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 #endif
