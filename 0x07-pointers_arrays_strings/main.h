4cfa2795d24a7418326919ed28b371300b7dba9f
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-09-26 16:18:49 -0700

    task 7

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,5 +6,7 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 void print_chessboard(char (*a)[8]);
 int _putchar(char c);
+unsigned int _strspn(char *s, char *accept);
+void print_diagsums(int *a, int size);
 
 #endif
