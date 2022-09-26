1d2d646ff1d2dd3b47e2dcec1e3c8f20a3a62967
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-09-26 15:30:28 -0700

    submit task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - locates the character in a string
+ * @s: string to be scanned
+ * @c: character to be searched
+ *
+ * Return: s if character is located,
+ * NULL if character is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return ((char *) s);
+		s++;
+	}
+
+	return ('\0');
+}
