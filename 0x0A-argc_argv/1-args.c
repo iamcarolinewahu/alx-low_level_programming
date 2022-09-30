a20f46375a3179731aef14f46373384ba358626e
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-09-30 11:04:01 -0700

    submitting task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main: prints the no. of arguments passed into it
+ * @argc: no of arguments
+ * @argv: arguments entered
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+
+}
