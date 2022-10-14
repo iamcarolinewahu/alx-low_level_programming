747ce8fde029731212cacfff63ad5321fe4048ec
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-10-14 12:26:00 -0700

    submit tast 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,9 +15,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	unsigned int x;
 	va_list num;
+	
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
 
 	va_start(num, n);
-
+	
 	for (i = 0; i < n; i++)
 	{
 		x = va_arg(num, int);
