2dd03bf8663171ac0bd85330d48b13df39ac8998
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-10-14 12:27:45 -0700

    solved trailing whitespace error

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,7 +15,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	unsigned int x;
 	va_list num;
-	
+
 	if (n == 0)
 	{
 		printf("\n");
@@ -23,7 +23,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	}
 
 	va_start(num, n);
-	
+
 	for (i = 0; i < n; i++)
 	{
 		x = va_arg(num, int);
