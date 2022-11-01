f15d267f1dabb69bd44a1c3a6cf0ea55ed77014f
iamcarolinewahu <caroline.wahu254@gmail.com>
2022-11-01 02:02:49 -0700

    modified header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,8 +5,13 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_char(va_list args);
-void print_int(va_list args);
-void print_float(va_list args);
-void print_string(va_list args);
+int _putchar(char c);
+
+typedef struct op
+{
+	char c;
+	void (*f)(va_list);
+} op_t;
+
+
 #endif
